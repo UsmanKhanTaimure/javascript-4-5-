f75cfd45e3427d2de9e4bffc49b39c92297caf91
UsmanKhanTaimure <107642292+UsmanKhanTaimure@users.noreply.github.com>
2022-06-16 10:51:09 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,100 @@
+// Chapter 5
+
+// Question 1
+
+// var num1 = 3 ;
+// var num2 = 5 ;
+// var result = 3 + 5;
+// document.write("Sum of 3 and 5 is " + result + "<br>")
+
+// Question 2
+// var num1 = 3 ;
+// var num2 = 5 ;
+// var result = 3 - 5;
+// document.write("subtraction of 3 and 5 is " + result + "<br>")
+
+// var num1 = 3 ;
+// var num2 = 5 ;
+// var result = 3 * 5;
+// document.write("Multiple of 3 and 5 is " + result + "<br>")
+
+// var num1 = 3 ;
+// var num2 = 5 ;
+// var result = 3 / 5;
+// document.write("Divided of 3 and 5 is " + result + "<br>")
+
+// var num1 = 3 ;
+// var num2 = 5 ;
+// var result = 3 % 5;
+// document.write("Modulus of 3 and 5 is " + result + "<br>")
+
+//  Question 3 
+// var num;
+// num = 5;
+// document.write("Initial value :" + num +"<br>");
+// var num1 = ++num;
+// document.write("Value After Increment :" + num1 +"<br>");
+// num1 +=7;
+// document.write("Value After Addition : " + num1  +"<br>")
+// --num1;
+// document.write("Value After Decrement : " + num1 +"<br>")
+// num1%=3;
+// document.write("The Remainder is : "  + num1)
+
+// Question 4
+// var Movie = 600;
+// var buyTick = 5
+// document.write("Total cost to buy 5 tikets to a movies is " + Movie*buyTick +"PKR");
+
+// Question 5;
+// document.write("<h1>MATHEMATICS TABLES</h1>");
+// var table = +prompt("ENTER YOUR TABLE NUMBER");
+// document.write(table + "x" + 1 + "=" + table * 1 + "<br>"
+//     + table + "x" + 2 + "=" + table * 2 + "<br>" + table + "x" + 3 + "=" + table * 3 + "<br>" + table + "x" + 4 + "=" + table * 4 + "<br>" + table + "x" + 5 + "=" + table * 5 + "<br>" + table + "x" + 6 + "=" + table * 6 + "<br>" + table + "x" + 7 + "=" + table * 7 + "<br>" + table + "x" + 8 + "=" + table * 8 + "<br>" + table + "x" + 9 + "=" + table * 9 + "<br>" +
+//     table + "x" + 10 + "=" + table * 10)
+
+// Question 6;
+
+// document.write("<h1>Shopping Cart</h1>");
+// var price_of_item_1 = 650;
+// var price_of_item_2 = 100*2;
+// var Quantity_of_item1 = 3;
+// var Quantity_of_item2 = 7*2;
+// var shippingCharges = 100;
+// document.write("Price of item 1 is : " + price_of_item_1 + "<br>");
+// document.write("Price of item 2 is : " + price_of_item_2 + "<br>");
+// document.write("Quantity of item 1 is : " + Quantity_of_item1 + "<br>");
+// document.write("Quantity of item 2 is : " + Quantity_of_item2 + "<br>"); 
+// document.write("Shipping Charges : " + shippingCharges  + "<br>"); 
+// console.log("Total price cost is " ,+price_of_item_1+price_of_item_2+Quantity_of_item1+Quantity_of_item2+shippingCharges)
+
+// Question 7;
+// document.write("<h1>Marks Sheet</h1>");
+// var totalmark = 980;
+// var MarkObtained = 804;
+// var perc = 804/980*100;
+// document.write("Total Marks is : " +totalmark + "<br>");
+// document.write("MarksObtained is : " +MarkObtained + "<br>");
+// document.write("Total Marks is : " + perc)
+
+// Question 8;
+// var usdoll = 10;
+// var sd = 25;
+// var pakus = 104.80;
+// var paksd = 28;
+// var cal = 10*104.80 + 25*28;
+// document.write("Total Currency is PKR : " +cal)
+
+// Question 9;
+// var num = 6;
+// var total = (num+5*10)/2;
+// document.write(total)
+
+// Question 10;
+// var curryear = 2022;
+// var birthYear = 2003;
+// var cal = curryear-birthYear;
+// document.write("Current Year : " + curryear + "<br>")
+// document.write("Birth Year : "  + birthYear + "<br>")
+// document.write("Your Age is : " +cal + "<br>")
+
